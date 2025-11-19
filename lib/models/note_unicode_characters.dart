6ab7bbbd2eebb40f73e4199f902727d00b22f015
rory1473 <roryhayden147@live.co.uk>
2025-11-19 23:14:36 +0000

    Fixed issue where selected character doesn't keep selected styling

--- lib/models/note_unicode_characters.dart
+++ lib/models/note_unicode_characters.dart
@@ -3,4 +3,15 @@ class NoteUnicodeCharacters {
   String upsideDown;
 
   NoteUnicodeCharacters({this.normal = '', this.upsideDown = ''});
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is NoteUnicodeCharacters &&
+        normal == other.normal &&
+        upsideDown == other.upsideDown;
+  }
+
+  @override
+  int get hashCode => normal.hashCode ^ upsideDown.hashCode;
 }
